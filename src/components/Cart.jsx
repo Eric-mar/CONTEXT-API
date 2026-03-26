abe876b96b014edd50c2a7db5aeaddcbd4bcac5b
tuyishime eric <erictuyishime2004@gmail.com>
2026-03-26 11:11:14 +0200

    allowing items to appear to the cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,33 +2,33 @@ import React, { useEffect, useState } from 'react'
 import SingleProduct from './SingleProduct'
 import './styles.css'
 
-function Cart({cart,setCart}) {
-    const [total,setTotal] = useState(0)
+function Cart({ cart, setCart }) {
+    const [total, setTotal] = useState(0)
 
-    useEffect(()=>
-        setTotal(cart.reduce((acc,curr)=> acc + Number(curr.price),0))
+    useEffect(() =>
+        setTotal(cart.reduce((acc, curr) => acc + Number(curr.price), 0))
 
-        
-        ,[cart])
+
+        , [cart]
 
   return (
-    <div>
-        <span style={{fontSize:30 }}>
-            My cart
-        </span>
-        <br/>
-        <span style={{fontSize: 30}}>
-            Total:{total}</span>
-        <div className='productContainer'>
-            {
-                cart.map((prod)=>(
-                    <SingleProduct prod={prod} key={prod.id} 
-                    cart={cart} setCart={setCart} />
-                ))
-            }
+        <div>
+            <span style={{ fontSize: 30 }}>
+                My cart
+            </span>
+            <br />
+            <span style={{ fontSize: 30 }}>
+                Total:{total}</span>
+            <div className='productContainer'>
+                {
+                    cart.map((prod) => (
+                        <SingleProduct prod={prod} key={prod.id}
+                            cart={cart} setCart={setCart} />
+                    ))
+                }
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Cart
